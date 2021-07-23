05a5a69ba447704e20d5b062c0760409b17a0781
Shekhar Singh <shekharsingh1997@gmail.com>
2021-07-23 11:24:37 +0200

    Combined the separation and sum functions

--- app/factorial-digits.py
+++ app/factorial-digits.py
@@ -10,21 +10,24 @@ import numpy as np
 import sys
 
 # Function to seperate the digits of the factorial:
-def sep_func(fact_input):
+def sep_sum_func(input_fact):
     sep_array = []
-    while fact_input !=0:
-        x = np.mod(fact_input, 10)      # Dividing by 10, the mod is separating the value
-        fact_input = fact_input//10     # Updating fact_input with the quotient o dividing by 10
-        sep_array.insert(0,x)           # adding the remainder value for each loop until fact_input reaches zero
-    return sep_array
+    # 1. Separating digits:
+    while input_fact !=0:
+        x = np.mod(input_fact, 10)      # Dividing by 10, the mod is separating the value
+        input_fact = input_fact//10     # Updating input_fact with the quotient o dividing by 10
+        sep_array.insert(0,x)           # adding the remainder value for each loop until input_fact reaches zero
+
+    # 2. Summing values:
+    fact_sum = np.sum(sep_array)
+    return fact_sum
 
 def main(user_input):
     # 1. Calculate factorial of input number
-    fact_input = np.math.factorial(user_input)
+    input_fact = np.math.factorial(user_input)
 
     # 2. Separate digits and sum them:
-    sep_digits = sep_func(fact_input)
-    fact_sum = np.sum(sep_digits)
+    fact_sum = sep_sum_func(input_fact)
 
     # 3. Print output value:
     print(fact_sum)
